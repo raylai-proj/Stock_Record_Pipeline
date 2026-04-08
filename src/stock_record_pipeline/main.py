e661d2bc5a0355703fc280e127bf0a093fb02212
raylai <chunjueilai@gmail.com>
2026-04-08 20:07:06 +0800

    feat: Add new main.py entry point

---
+++ src/stock_record_pipeline/main.py
@@ -0,0 +1,14 @@
+"""
+Summary: main.py launch the main() process stock record pipeline.
+
+Author: Chun-Juei Lai 04/08/2026
+"""
+
+
+def main():
+    """Entry point of the program."""
+    print("processing stock record")
+
+
+if __name__ == "__main__":
+    main()
