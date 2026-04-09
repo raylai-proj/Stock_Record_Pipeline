8b9577a5a4c6c6791a684795f940e566416d06d2
raylai <chunjueilai@gmail.com>
2026-04-09 15:17:52 +0800

    feat: create nwe folder utility and new file window_helper.py

---
+++ src/stock_record_pipeline/utility/window_helper.py
@@ -0,0 +1,8 @@
+"""
+Summary: window_helper.py creates a GUI for selecting and executing analysis actions.
+
+This module defines a Components class that builds a basic tkinter-based interface
+with a dropdown to choose miscellaneous options analyze data.
+
+Chun-Juei Lai 04/09/2026
+"""
